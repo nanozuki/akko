1729c13bbef4d2309215365639b219ddba33a7a4
Nanozuki Crows <nanozuki.crows@gmail.com>
2023-07-10 21:07:28 +0800

    feat: start to add parser to read files

--- parser.go
+++ parser.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"reflect"
 	"strings"
 )
 
@@ -16,46 +17,69 @@ func ParseService(dir string, structName string) (*ServiceInfo, error) {
 		return nil, fmt.Errorf("failed to parse dir: %w", err)
 	}
 	pkgCount := 0
+	pkgName := ""
 	for pkg := range pkgs {
 		if !strings.HasPrefix(pkg, "_test") {
 			pkgCount++
+			pkgName = pkg
 		}
 	}
 	if len(pkgs) > 1 {
 		return nil, fmt.Errorf("more than 1 package found in dir %s", dir)
 	}
-	return nil, nil
+	return parsePackage(pkgs[pkgName], structName)
 }
 
 func parsePackage(pkg *ast.Package, structName string) (*ServiceInfo, error) {
 	var serviceDecl *ast.TypeSpec
+	var methods []*ast.FuncDecl
 	ast.Inspect(pkg, func(node ast.Node) bool {
 		switch node := node.(type) {
 		case *ast.TypeSpec:
-			st, ok := node.Type.(*ast.StructType)
-			if ok && node.Name.Name == structName {
+			if _, ok := node.Type.(*ast.StructType); ok && node.Name.Name == structName {
 				serviceDecl = node
-				return false
 			}
 		case *ast.FuncDecl:
-			if node.Recv != nil && node.Recv.List[0].
-		}
-		return false
-	})
-	for _, file := range pkg.Files {
-		for _, decl := range file.Decls {
-			switch decl := decl.(type) {
-			case *ast.GenDecl:
-				for _, spec := range decl.Specs {
-					switch spec := spec.(type) {
-					case *ast.TypeSpec:
-						if spec.Name.Name == structName {
-							return parseStruct(spec.Type.(*ast.StructType))
-						}
+			if node.Recv != nil {
+				switch recv := node.Recv.List[0].Type.(type) {
+				case *ast.StarExpr:
+					if recv.X.(*ast.Ident).Name == structName {
+						methods = append(methods, node)
+					}
+				case *ast.Ident:
+					if recv.Obj.Name == structName {
+						methods = append(methods, node)
 					}
 				}
 			}
 		}
+		return false
+	})
+	if serviceDecl == nil || len(methods) == 0 {
+		return nil, fmt.Errorf("struct %s not found", structName)
 	}
-	return nil, fmt.Errorf("struct %s not found", structName)
+	si := ServiceInfo{
+		Typ: TypeInfo{
+			Name:      structName,
+			PkgPath:   "", // TODO
+			Indentity: fmt.Sprintf("%s.%s", pkg.Name, structName),
+			Kind:      reflect.Struct, // TODO
+		},
+	}
+	for _, method := range methods {
+		makeMethodInfo(&si, serviceDecl, method)
+	}
+	return &si, nil
+}
+
+func makeMethodInfo(si *ServiceInfo, serviceDecl *ast.TypeSpec, method *ast.FuncDecl) {
+	mi := MethodInfo{
+		Name:       method.Name.Name,
+		Inputs:     []ParamInfo{},
+		Returns:    []ParamInfo{},
+		HTTPMethod: "",
+		URL:        "",
+		Process:    []Process{},
+	}
+	si.Methods = append(si.Methods, mi)
 }
